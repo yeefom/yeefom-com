990c9f7d44623a994834c0086c07153b7d0f3fe3
Yifeng Wang <yifeng@yeefom.com>
2020-05-19 21:46:24 -0700

    Clean up 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,15 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
 import Layout from '../components/Layout'
 import Container from '../components/Container'
 import i18n from '../i18n'
 
-export default ({ data: { site } }) => {
-  return (
-    <Layout site={site} pageTitle='404'>
-      <Container>
-          <span css={css`margin-bottom: 40px;`}>
-            {i18n.notFound}
-          </span>
-      </Container>
-    </Layout>
-  );
-}
-
-export const pageQuery = graphql`
-  query {
-    site {
-      ...site
-    }
-  }
-`
+export default () => (
+  <Layout pageTitle='404'>
+    <Container>
+        <span css={css`margin-bottom: 40px;`}>
+          {i18n.notFound}
+        </span>
+    </Container>
+  </Layout>
+)
