7adf52098e86843d773d3b964026fdc0e86031e6
Yifeng Wang <yifeng@yeefom.com>
2020-05-20 20:02:28 -0700

    Use pageQuery in pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,45 +1,43 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { css } from '@emotion/core'
 import Container from '../components/Container'
 import Layout from '../components/Layout'
 import SEO from "../components/SEO";
 
-export default () => {
-  const { mdx: { fields, body } } = useStaticQuery(graphql`
-    query {
-      mdx(
-        fields: { type: { eq: "page" }, slug: { eq: "about" } }, 
-      ) {
-        fields {
-          title
-        }
-        body
+export default ({ data: { mdx: { fields, body } } }) => (
+  <Layout pageTitle={fields.title}>
+    <SEO postMeta={{title: fields.title}}/>
+    <Container css={css`
+      p:last-of-type {
+        margin-bottom: 0;
       }
-    }
-  `)
+    `}>
+      <article>
+        <h1
+          css={css`
+            margin-bottom: 40px;
+          `}
+        >
+          {fields.title}
+        </h1>
+        <br/>
+        <MDXRenderer>{body}</MDXRenderer>
+      </article>
+    </Container>
+  </Layout>
+)
 
-  return (
-    <Layout pageTitle={fields.title}>
-      <SEO postMeta={{ title: fields.title }} />
-      <Container css={css`
-        p:last-of-type {
-          margin-bottom: 0;
-        }
-      `}>
-        <article>
-          <h1
-            css={css`
-              margin-bottom: 40px;
-            `}
-          >
-            {fields.title}
-          </h1>
-          <br />
-          <MDXRenderer>{body}</MDXRenderer>
-        </article>
-      </Container>
-    </Layout>
-  )
-}
+export const pageQuery = graphql`
+  query {
+    mdx(
+      fields: { type: { eq: "page" }, slug: { eq: "about" } }
+    ) {
+      fields {
+        title
+      }
+      body
+    }
+  }
+`
--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
 import Container from '../components/Container'
 import SEO from '../components/SEO'
@@ -7,67 +7,65 @@ import Layout from '../components/Layout'
 import Link from '../components/Link'
 import i18n from '../i18n'
 
-export default () => {
-  const { allMdx } = useStaticQuery(graphql`
-    query {
-      allMdx(
-        sort: { fields: [fields___date], order: DESC }
-        filter: { fields: { type: { eq: "blog" } } }
-      ) {
-        edges {
-          node {
-            id
-            fields {
-              pagePath
-              title
-              date(formatString: "MMMM DD, YYYY")
-            }
+export default ({ data: { allMdx } }) => (
+  <Layout pageTitle={i18n.archive}>
+    <SEO postMeta={{title: i18n.archive}}/>
+    <Container>
+      <h1 css={css`margin-bottom: 40px;`}>
+        {i18n.archive}
+      </h1>
+      <br/>
+      {allMdx.edges.map(({node}) => {
+        const {fields, id} = node
+
+        return (
+          <div
+            key={id}
+            css={css`
+              :not(:first-of-type) {
+                margin-top: 20px;
+              }
+              display: flex;
+              flex-direction: column;
+            `}
+          >
+            <small>{fields.date}</small>
+            <h2
+              css={css`
+                font-size: 1em;
+                margin-top: 8px;
+                margin-bottom: 10px;
+              `}
+            >
+              <Link
+                aria-label={`View ${fields.title} article`}
+                to={fields.pagePath}>
+                {fields.title}
+              </Link>
+            </h2>
+          </div>
+        );
+      })}
+    </Container>
+  </Layout>
+)
+
+export const pageQuery = graphql`
+  query {
+    allMdx(
+      sort: { fields: [fields___date], order: DESC }
+      filter: { fields: { type: { eq: "blog" } } }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            pagePath
+            title
+            date(formatString: "MMMM DD, YYYY")
           }
         }
       }
     }
-  `)
-
-  return (
-    <Layout pageTitle={i18n.archive}>
-      <SEO postMeta={{ title: i18n.archive }}/>
-      <Container>
-        <h1 css={css`margin-bottom: 40px;`}>
-          {i18n.archive}
-        </h1>
-        <br />
-        {allMdx.edges.map(({ node }) => {
-          const { fields, id } = node
-
-          return (
-            <div
-              key={id}
-              css={css`
-                :not(:first-of-type) {
-                  margin-top: 20px;
-                }
-                display: flex;
-                flex-direction: column;
-              `}
-            >
-              <small>{fields.date}</small>
-              <h2
-                css={css`
-                  font-size: 1em;
-                  margin-top: 8px;
-                  margin-bottom: 10px;
-                `}
-              >
-                <Link
-                  aria-label={`View ${fields.title} article`}
-                  to={fields.pagePath}>
-                  {fields.title}
-                </Link>
-              </h2>
-            </div>
-          );
-        })}
-      </Container>
-    </Layout>
-  )
-}
+  }
+`
