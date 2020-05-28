a83381f6c60709ec84801b354197ed11fe8be6f8
Yifeng Wang <yifeng@yeefom.com>
2020-05-28 15:03:01 -0700

    Remove html.js and improve Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -31,26 +31,25 @@ export default ({ children, pageTitle, pageName, pageIndex }) => {
 
   return (
     <Fragment>
+      <Helmet title={siteTitle}>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=JyB3qeXgBY" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=JyB3qeXgBY" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=JyB3qeXgBY" />
+        <link rel="manifest" href="/site.webmanifest?v=JyB3qeXgBY" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=JyB3qeXgBY" color="#2e2f30" />
+        <link rel="shortcut icon" href="/favicon.ico?v=JyB3qeXgBY" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#2e2f30" />
+      </Helmet>
       <Global styles={reset()} />
       <Global styles={getGlobalStyles()} />
       <Global styles={littleFoot()} />
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          width: 100%;
-          min-height: 100vh;
-        `}
-      >
-        <Helmet title={siteTitle}>
-          <html lang="en" />
-        </Helmet>
-        <Header pageName={pageName}/>
-        <MDXProvider components={{ code: Code }}>
-          <Fragment>{children}</Fragment>
-        </MDXProvider>
-        <Footer/>
-      </div>
+
+      <Header pageName={pageName}/>
+      <MDXProvider components={{ code: Code }}>
+        <Fragment>{children}</Fragment>
+      </MDXProvider>
+      <Footer/>
     </Fragment>
   )
 }
